f98e1b9a431382c61c68b9d124b2d8dc00b34bad
AliesYangpai <yangpai_beibei@163.com>
2020-08-30 10:29:11 +0800

    加入资源文件，加入navigation

--- app/src/main/java/org/alieoa/work/ui/MainActivity.kt
+++ app/src/main/java/org/alieoa/work/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package org.alieoa.work.ui
 
-
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.alieoa.basemvp.BaseActivity
 import org.alieoa.work.R
 import org.alieoa.work.contract.MainContract
@@ -8,7 +11,8 @@ import org.alieoa.work.contract.presenter.PresenterMain
 
 class MainActivity : BaseActivity<MainContract.IMainView,PresenterMain>(),MainContract.IMainView {
 
-
+    lateinit var mBottomNavigationView: BottomNavigationView
+    lateinit var mNavController: NavController
     override fun layoutId(): Int {
         return R.layout.activity_main
     }
@@ -18,6 +22,9 @@ class MainActivity : BaseActivity<MainContract.IMainView,PresenterMain>(),MainCo
     }
 
     override fun initView() {
+        mBottomNavigationView = findViewById(R.id.bnv)
+        mNavController = findNavController(R.id.fg_main_container)  // 找NavController
+        NavigationUI.setupWithNavController(mBottomNavigationView,mNavController)
     }
 
     override fun initListener() {
