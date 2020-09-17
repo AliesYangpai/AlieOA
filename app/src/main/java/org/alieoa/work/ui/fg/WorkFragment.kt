47a0432f9449961e307ce1ab684c7cf29b39b757
AliesYangpai <yangpai_beibei@163.com>
2020-09-17 20:57:37 +0800

    fix butterknife in kt,add screen fit sw-xxx

--- app/src/main/java/org/alieoa/work/ui/fg/WorkFragment.kt
+++ app/src/main/java/org/alieoa/work/ui/fg/WorkFragment.kt
@@ -5,15 +5,12 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import butterknife.BindView
 import butterknife.OnClick
 import com.google.android.material.tabs.TabLayout
 import org.alieoa.basemvp.BaseFragment
-import org.alieoa.work.AlieOaApplication
 import org.alieoa.work.R
 
 import org.alieoa.work.contract.WorkContract
@@ -22,29 +19,31 @@ import org.alieoa.work.contract.presenter.PresenterFgWork
 class WorkFragment : BaseFragment<WorkContract.IWorkView, PresenterFgWork>(),
     WorkContract.IWorkView {
 
+    lateinit var mNavController: NavController
+
     @BindView(R.id.tv_left)
-    lateinit var mTvLeft:TextView
+    lateinit var mTvLeft: TextView
 
     @BindView(R.id.iv_right)
-    lateinit var mIvRight:ImageView
+    lateinit var mIvRight: ImageView
 
     @BindView(R.id.tbl_work_child)
-    lateinit var mTblWorkChild:TabLayout
-
-    @BindView(R.id.fg_work_child_container)
-    lateinit var mFgWorkChildContainer:Fragment
+    lateinit var mTblWorkChild: TabLayout
 
     @BindView(R.id.iv_add)
-    lateinit var mIvAdd:ImageView
+    lateinit var mIvAdd: ImageView
 
-    @OnClick(R.id.iv_right,
-        R.id.iv_add)
-    fun onClick(view:View) {
-        when (view.id){
-            R.id.iv_right->
-                Toast.makeText(mActivity.applicationContext,"iv_right",Toast.LENGTH_SHORT).show()
+
+    @OnClick(
+        R.id.iv_right,
+        R.id.iv_add
+    )
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.iv_right ->
+                Toast.makeText(mActivity.applicationContext, "iv_right", Toast.LENGTH_SHORT).show()
             R.id.iv_add ->
-                Toast.makeText(mActivity.applicationContext,"iv_add",Toast.LENGTH_SHORT).show()
+                Toast.makeText(mActivity.applicationContext, "iv_add", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -57,9 +56,15 @@ class WorkFragment : BaseFragment<WorkContract.IWorkView, PresenterFgWork>(),
     }
 
     override fun initView(rootView: View) {
-        val navController = findNavController()
+        mNavController = findNavController()
+        mTblWorkChild.addTab(mTblWorkChild.newTab().setText(getString(R.string.work_all)))
+        mTblWorkChild.addTab(mTblWorkChild.newTab().setText(getString(R.string.approve)))
+        mTblWorkChild.addTab(mTblWorkChild.newTab().setText(getString(R.string.work_report)))
+        mTblWorkChild.addTab(mTblWorkChild.newTab().setText(getString(R.string.work_client)))
+        mTblWorkChild.addTab(mTblWorkChild.newTab().setText(getString(R.string.work_notice)))
 
     }
+
 
     override fun initListener() {
     }
