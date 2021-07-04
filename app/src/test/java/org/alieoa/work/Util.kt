136dc42d475ef9ed762ef646ab5dc929e992380e
AliesYangpai <yangpai_beibei@163.com>
2021-07-04 10:42:01 +0800

    first commit

---
+++ app/src/test/java/org/alieoa/work/Util.kt
@@ -0,0 +1,18 @@
+package org.alieoa.work
+
+/**
+ * Created by Administrator on 2020/8/29 0029.
+ * 类描述
+ * 版本
+ */
+//object Util {
+//    val name = "xxxx"
+//    fun play(){}
+//}
+
+class Util {
+    companion object {
+        val name = "xxxx"
+        fun play(){}
+    }
+}
