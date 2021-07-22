d3601c48ffc9c09af1ff8443cf33bdc816e9e994
AliesYangpai <yangpai_beibei@163.com>
2021-07-22 22:33:37 +0800

    add some lib

--- app/src/main/java/org/alieoa/work/MainActivity.kt
+++ app/src/main/java/org/alieoa/work/MainActivity.kt
@@ -3,17 +3,19 @@ package org.alieoa.work
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.alieoa.work.databinding.ActivityMainBinding
 import org.alieoa.work.test.TestActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
         showTest()
     }
 
     private fun showTest() {
-        startActivity(Intent(this,TestActivity::class.java))
+        startActivity(Intent(this, TestActivity::class.java))
     }
 }
